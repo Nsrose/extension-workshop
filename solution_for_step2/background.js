b32a72575f28957644b5177075cf27fd59547681
nsrose <nsrose@berkeley.edu>
2014-04-21 16:55:54 -0700

    added

---
+++ solution_for_step2/background.js
@@ -0,0 +1,60 @@
+/**
+ * Listens for the app launching then creates the window
+ *
+ * @see http://developer.chrome.com/apps/app.window.html
+ */
+
+function launch() {
+ chrome.app.window.create('index.html', {
+   id: 'main',
+   bounds: { width: 620, height: 500 }
+ });
+}
+
+
+
+
+
+var dbName = 'todos-vanillajs';
+
+function showNotification(storedData) {
+
+  var openTodos = 0;
+
+  if ( storedData[dbName].todos ) {
+    storedData[dbName].todos.forEach(function(todo) {
+      if ( !todo.completed ) {
+        openTodos++;
+      }
+    });
+  }
+
+  if (openTodos>0) {
+    // Now create the notification
+    chrome.notifications.create('reminder', {
+        type: 'basic',
+        iconUrl: 'icon_128.png',
+        title: 'Don\'t forget!',
+        message: 'You have '+openTodos+
+                 ' things to do. Wake up, dude!'
+     }, function(notificationId) {})
+  }
+}
+
+// When the user clicks on the notification,
+// we will open the To Do list and dismiss the notification
+chrome.notifications.onClicked.addListener(
+  function( notificationId ) {
+    launch();
+    chrome.notifications.clear(notificationId, function() {});
+  }
+);
+
+chrome.app.runtime.onLaunched.addListener(launch);
+
+chrome.alarms.onAlarm.addListener(function( alarm ) {
+ // console.log("Got an alarm!", alarm);
+ chrome.storage.local.get(dbName, showNotification);
+
+});
+
